4700383733826cac47c06fbbd58b11fb939ad0df
khacthe <khacthe.python@gmail.com>
2019-07-16 13:28:07 +0700

    css header layout

--- src/components/Layouts/MainLayout.js
+++ src/components/Layouts/MainLayout.js
@@ -1,17 +1,22 @@
 // @flow
 import * as React from 'react';
+import styled from 'styled-components';
+
 import { Sidebar, Header, Footer } from '.';
 
+const Body = styled.div`
+  background: #fafafa;
+`;
 
 const  MainLayout = (props) => {
   const { children } = props;
     return (
-      <main>
+      <Body>
         <Header />
         <Sidebar />
         {children}
         <Footer />
-      </main>
+      </Body>
     );
   }
 
